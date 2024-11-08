ae8a9077a6c550ad53af45af32d186b85482e1ca
Miscanthus53 <s.yutaka1985@gmail.com>
2024-11-08 15:13:01 +0900

    修正

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -2,9 +2,7 @@ package kadai_018;
 
 public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
 	
-	public KatoIchiro_Chapter18(){
-		setGivenName();
-	}
+
 	
 	public void setGivenName() {
 		this.givenName = "一郎";
